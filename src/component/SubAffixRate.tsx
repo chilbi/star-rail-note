4ca4e17a8e8fc1034dcd8134bf9f0832cf4c673c
chilbi <13232158717@163.com>
2024-02-10 12:57:38 +0800

    update layout

--- src/component/SubAffixRate.tsx
+++ src/component/SubAffixRate.tsx
@@ -1,6 +1,7 @@
 import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
 import StarHalfRoundedIcon from '@mui/icons-material/StarHalfRounded';
 import StarRoundedIcon from '@mui/icons-material/StarRounded';
+import Box from '@mui/joy/Box';
 
 interface SubAffixRateProps {
   count: number;
@@ -9,7 +10,7 @@ interface SubAffixRateProps {
 
 export default function SubAffixRate({ count, step }: SubAffixRateProps) {
   return (
-    <>
+    <Box component="span" display="inline-flex" ml="3.5px">
       {Array(count).fill(null).map((_, i) => {
         const rate = step - i * 2;
         const Icon = rate > 1 ? StarRoundedIcon : rate > 0 ? StarHalfRoundedIcon : StarBorderRoundedIcon;
@@ -17,11 +18,10 @@ export default function SubAffixRate({ count, step }: SubAffixRateProps) {
           <Icon
             key={i}
             color="warning"
-            style={{ transform: `translateX(-${3.5 * i}px)` }}
-            sx={{  width: '12px', height: '12px' }}
+            sx={{ ml: '-3.5px', width: '12px', height: '12px' }}
           />
         );
       })}
-    </>
+    </Box>
   );
 }
